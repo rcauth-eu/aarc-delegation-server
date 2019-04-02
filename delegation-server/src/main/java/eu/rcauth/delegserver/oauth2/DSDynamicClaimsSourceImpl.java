51b26cda9d7488976ecf7d4c9c6efa6653481eaf
Mischa Salle <msalle@nikhef.nl>
2019-04-02 18:34:39 +0200

    Cleanup and clarify DSDynamicClaimsSourceImpl
    The DSDynamicClaimsSourceImpl is required in the DS to produce the list of
    supported scopes in the .well-known discovery page. Put lots of clarifying text
    in the file and remove the overridden methods we shouldn't override since they
    aren't (shouldn't be) used in any case.

--- delegation-server/src/main/java/eu/rcauth/delegserver/oauth2/DSDynamicClaimsSourceImpl.java
+++ delegation-server/src/main/java/eu/rcauth/delegserver/oauth2/DSDynamicClaimsSourceImpl.java
@@ -8,91 +8,62 @@ import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.BasicClaimsSourceImpl;
 import edu.uiuc.ncsa.security.delegation.server.ServiceTransaction;
 import net.sf.json.JSONObject;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import edu.uiuc.ncsa.security.servlet.PresentableState;
+import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.OA2AuthorizationServer;
+import edu.uiuc.ncsa.myproxy.oa4mp.server.servlet.AbstractAuthorizationServlet;
+import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.OA2DiscoveryServlet;
+import edu.uiuc.ncsa.security.oauth_2_0.server.claims.ClaimSourceConfiguration;
+import eu.rcauth.delegserver.oauth2.servlet.DSOA2AuthorizationServer;
+import edu.uiuc.ncsa.security.oauth_2_0.server.config.LDAPConfiguration;
+
 // TODO add imports for javadoc
 
 /**
- * Custom ClaimsSourceImpl that adds claims into UserInfo and IDToken.
+ * Custom ClaimsSourceImpl that adds the list of supported claims into the UserInfo.
+ * This is done in {@link OA2DiscoveryServlet#setValues(HttpServletRequest, JSONObject)}
+ * using oa2SE.getClaimSource().getClaims(). Hence we override here {@link #getClaims()}.
  * <br>
- * TODO we need to decide whether we want to allow additional handlers, which
- * then also need additional configuration via a {@link ClaimSourceConfiguration}.
- * Currently, all the extra claims from the DS are already forwarded and put in
- * the transaction in
- * {@link eu.rcauth.delegationserver.server.servlet.DSOA2AuthorizationServer#prepare(PresentableState)}
- * by
- * {@link OA2ServiceTransaction#setClaims(JSONObject)}.
+ * For the ID token, all the (extra) claims are already put in
+ * the transaction by
+ * {@link DSOA2AuthorizationServer#generateClaims(AuthorizedState)}
+ * in
+ * {@link DSOA2AuthorizationServer#prepare(PresentableState)}
+ * Doing this instead by overriding process here would not help as it would only
+ * run after the consent page with the claims is already shown (in
+ * {@link OA2AuthorizationServer#createRedirect(HttpServletRequest, HttpServletResponse, ServiceTransaction)}
+ * called from {@link AbstractAuthorizationServlet#doIt(HttpServletRequest, HttpServletResponse)}
+ * with {@link AbstractAuthorizationServlet#AUTHORIZATION_ACTION_OK}).
  * <br>
- * NOTE: this ClaimsSourceImpl is called 'on the way' back, when returning from
- * the mp-server to the (e.g. vo-portal) client. It is called by
- * {@link OA2ClaimsUtil#createBasicClaims(HttpServletRequest, OA2ServiceTransaction)},
- * called from
- * {@link DSOA2AuthorizationServer#createRedirect(HttpServletRequest, HttpServletResponse, ServiceTransaction)}.
- * It is only invoked if both {@link #isEnabled()} and
+ * Additionally it is only invoked if both {@link #isEnabled()} and
  * {@link #isRunAtAuthorization()} are true
  * ({@link BasicClaimsSourceImpl#isEnabled()} returns true if it has a valid
  * {@link ClaimSourceConfiguration}, which additionally is set to enabled, see
  * also the constructor).
  *
  * @author "Tamás Balogh"
+ * @author "Mischa Sall&eacute;"
+ *
  */
 public class DSDynamicClaimsSourceImpl extends BasicClaimsSourceImpl    {
 
     /**
      * Constructor for a DSDynamicClaimsSourceImpl.
-     * It needs to have a {@link ClaimSourceConfiguration} which is enabled.
-     * See for example {@linkplain edu.uiuc.ncsa.security.oauth_2_0.server.config.LDAPConfiguration}
+     * We disable it for creating claims in the ID token, as that is handle
+     * elsewhere, by setting the configuration to null.
      */
     public DSDynamicClaimsSourceImpl() {
         /* We currently don't need this ClaimsSourceImpl. */
         this.setConfiguration(null);
-/*        ClaimSourceConfiguration claimSourceConfiguration = new ClaimSourceConfiguration();
+/*      ClaimSourceConfiguration claimSourceConfiguration = new ClaimSourceConfiguration();
         claimSourceConfiguration.setEnabled(true);
         this.setConfiguration(claimSourceConfiguration);*/
     }
 
-    @Override
-    // TODO Unclear what this does but it needs to be true or it's skipped in
-    // edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.OA2ClaimsUtil#createBasicClaims
-    public boolean isRunAtAuthorization(){
-        /* We currently don't use this ClaimsSourceImpl, so set to false. Also
-         * need to have a enabled non-null configuration in order to run */
-        return false;
-/*        return true;*/
-    }
-
-    /**
-     * Returns the {@link JSONObject} object passed to it completed with the claims saved by the
-     * corresponding transaction.
-     *
-     * @param claims being returned to the client
-     * @param transaction the current transaction record
-     * @return The UserInfo completed with claims
-     */
-    @Override
-    public JSONObject process(JSONObject claims, ServiceTransaction transaction) {
-
-        //add the claims build based on the requested scopes into the userinfo reply
-
-        //get claims previously extracted from the transaction
-        DSOA2ServiceTransaction t = (DSOA2ServiceTransaction) transaction;
-        JSONObject transClaims =  t.getClaims();
-
-        // TODO check logic
-        if ( transClaims != null ) {
-            //some claims might already by set by now, so instead of overwriting them,
-            //simple append the claims from the transaction.
-            for ( String claimKey : (Set<String>)transClaims.keySet()) {
-                claims.put(claimKey, transClaims.get(claimKey));
-            }
-        } else {
-            //don't fail on empty claims
-        }
-
-        return claims;
-    }
-
     /**
      * Return a flattened list of all the claims in all the scopes as configured
-     * in the <scopes> node
+     * in the <scopes> node. Used in the {@link OA2DiscoveryServlet}.
      * @return complete set of configured claims
      */
     @Override
